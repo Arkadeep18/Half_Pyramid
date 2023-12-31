05d9be95e9ff0fc728e60660bf2398b453dbb6ef
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-01 00:05:53 +0530

    Add files via upload

---
+++ Prac17.java
@@ -0,0 +1,14 @@
+
+public class Prac17 {
+
+	public static void main(String[] args) {
+		int x = 5;
+		for(int a = 1; a <=x;a++) {
+			for( int b = 1; b<=a; b++) {
+				System.out.print("*");
+			}
+				System.out.println();
+	}
+	}
+}
+
